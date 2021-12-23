afb5e3d55b826b9ecd4176612fe5279d64674dfd
fireWizard23 <62689803+fireWizard23@users.noreply.github.com>
2021-12-23 14:53:13 +0800

    Moved the http to joke-http

--- src/app/services/joke-http-service/joke-http.service.ts
+++ src/app/services/joke-http-service/joke-http.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { HttpService } from '../http-service/http-service.service';
+import { Observable, ReplaySubject, Subject } from 'rxjs';
 import { environment as env } from 'src/environments/environment';
-import { HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import {TwoPartJoke,Joke, SingleJoke, JokeCategory, JokeType} from '../../misc/joke.model';
 
 
@@ -10,8 +9,39 @@ import {TwoPartJoke,Joke, SingleJoke, JokeCategory, JokeType} from '../../misc/j
 @Injectable({
   providedIn: 'root'
 })
-export class JokeHttpService extends HttpService {
+export class JokeHttpService  {
 
+  private _currentJoke: Subject<Joke> = new ReplaySubject();
+
+  private _hasJoke = false;
+
+  get currentJoke() {
+      return this._currentJoke;
+  }
+
+  get hasJoke(){
+    return this._hasJoke;
+  }
+
+  constructor(protected http : HttpClient ) {
+    this.refreshCurrentJoke();
+  }
+
+  public refreshCurrentJoke() {
+    let random = Math.random();
+    this._hasJoke = true;
+    if(random > 0.5) {
+      this.getSingleJoke()
+        .subscribe((v) => {
+          this._currentJoke.next(v);
+        })
+    } else {
+      this.getTwoPartJoke()
+        .subscribe((v) => {
+          this._currentJoke.next(v);
+        })
+    }
+  }
 
   public getSingleJoke(category: JokeCategory='any') : Observable<SingleJoke> {
     return this.getRandomJoke<SingleJoke>('single', category) ;
@@ -39,6 +69,27 @@ export class JokeHttpService extends HttpService {
 
   } 
 
+  private getByUrl<T extends object>(url: string) : Observable<T> {
+    // let params = new HttpParams().set('type', 'single');
+    return this.http.get<T>(this.combineUrl(env.JOKE_URL, url));
+  }
+
+  private combineUrl(url1: string, url2: string ) : string {
+    const slash = '/';
+    const firstUrlEndsWithSlash = url1.endsWith(slash);
+    const secondUrlStartsWithSlash = url2.startsWith(slash);
+
+
+    if(!firstUrlEndsWithSlash && !secondUrlStartsWithSlash)
+      url1 += slash;
+    
+      return url1 + url2;
+
+  }
 
 
 }
+function tap(tap: any) {
+  throw new Error('Function not implemented.');
+}
+
