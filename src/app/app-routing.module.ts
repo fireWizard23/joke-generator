aa320bea03b930f3dcf834bde2a536c9040e1240
fireWizard23 <62689803+fireWizard23@users.noreply.github.com>
2021-12-26 21:56:09 +0800

    Added jokefilter to route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { JokeFilterComponent } from './components/joke-filter/joke-filter.component';
 import { JokePageComponent } from './components/joke-page/joke-page.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 
@@ -8,6 +9,10 @@ const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'joke/filter',
+    component: JokeFilterComponent
   },
   {
     path: 'joke/:id',
